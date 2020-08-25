21e60c5430d08686fa1a85a486efbd9490e27adc
Ulises López <ulises050794@gmail.com>
2020-08-24 18:51:14 -0600

    lista de post

--- django_with_examples/urls.py
+++ django_with_examples/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog'))
+    url(r'^blog/', include('blog.urls'))
 ]

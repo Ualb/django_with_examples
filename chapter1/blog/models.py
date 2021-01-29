881b8602539aaea902c8a054e38dedebd918bb53
Ulises López <ulises050794@gmail.com>
2021-01-28 20:50:33 -0600

    Reordenando a multilinea declaración

--- chapter1/blog/models.py
+++ chapter1/blog/models.py
@@ -15,16 +15,17 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250,
-    unique_for_date='publish')
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
-    related_name='blog_post')
+                            unique_for_date='publish')
+    author = models.ForeignKey(User, 
+                                on_delete=models.CASCADE,
+                                related_name='blog_post')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10,
-    choices=STATUS_CHOICES,
-    default='draft')
+                                choices=STATUS_CHOICES,
+                                default='draft')
 
     # modificando el query set mediante un manager
     objects = models.Manager() # el default
@@ -38,7 +39,7 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', 
-            args=[self.publish.year, 
-            self.publish.month, 
-            self.publish.day, 
-            self.slug])
+                        args=[self.publish.year, 
+                                self.publish.month, 
+                                self.publish.day, 
+                                self.slug])

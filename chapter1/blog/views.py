3de9ecd96df303d720d882466dbadde9a90d75b4
Ulises López <ulises050794@gmail.com>
2021-01-28 20:50:55 -0600

    Reordenando a multilinea declaración

--- chapter1/blog/views.py
+++ chapter1/blog/views.py
@@ -21,8 +21,12 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'page':page, 'posts': posts})
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
-    publish__month=month, publish__day=day)
+    post = get_object_or_404(Post, 
+                            slug=post, 
+                            status='published', 
+                            publish__year=year,
+                            publish__month=month, 
+                            publish__day=day)
     return render(request, 'blog/post/detail.html', {'post':post})
 
 def post_share(request, post_id):
